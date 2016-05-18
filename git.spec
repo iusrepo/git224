iusrepo/git224

commit 58fa1693f75e7c2bbf4fe4854214bbbda6a527ab

Set LDFLAGS for hardened builds (#1289728)
Without LDFLAGS the git exectuables were not being linked with the
desired settings for hardened builds.

git.spec
@@ -50,7 +50,7 @@
 
 Name:           git
 Version:        2.8.1
-Release:        2%{?dist}
+Release:        3%{?dist}
 Summary:        Fast Version Control System
 License:        GPLv2
 Group:          Development/Tools
@@ -385,6 +385,7 @@ find prebuilt_docs/html -type d | xargs rmdir --ignore-fail-on-non-empty
 cat << \EOF > config.mak
 V = 1
 CFLAGS = %{optflags}
+LDFLAGS = %{__global_ldflags}
 BLK_SHA1 = 1
 NEEDS_CRYPTO_WITH_SSL = 1
 USE_LIBPCRE = 1
@@ -729,6 +730,9 @@ rm -rf %{buildroot}
 # No files for you!
 
 %changelog
+* Mon Apr 11 2016 Todd Zullinger <tmz@pobox.com> - - 2.8.1-3
+- Set LDFLAGS for hardened builds (#1289728)
+
 * Wed Apr 06 2016 Paolo Bonzini <pbonzini@redhat.com> - 2.8.1-2
 - Install git-credentials-netrc (#1303358)
 

commit 72c240de2abb68f07e702db21bc841bfdc97bc68

Define __global_ldflags on EL < 7 (#1337137)
Building on EL was unintentionally broken by 58fa169 (Set LDFLAGS for
hardened builds (#1289728), 2016-04-11).

Thanks to Dimitry Andric for reporting this issue.

git.spec
@@ -48,6 +48,11 @@
 %global use_systemd         0
 %endif
 
+# Settings for EL <= 7
+%if 0%{?rhel} && 0%{?rhel} <= 7
+%{!?__global_ldflags: %global __global_ldflags -Wl,-z,relro}
+%endif
+
 Name:           git
 Version:        2.8.2
 Release:        4%{?dist}
@@ -732,6 +737,7 @@ rm -rf %{buildroot}
 %changelog
 * Wed May 18 2016 Todd Zullinger <tmz@pobox.com> - 2.8.2-4
 - Use perl(MOD::NAME) format for perl-DBD-SQLite and perl-Digest-MD5 deps
+- Define __global_ldflags on EL < 7 (#1337137)
 
 * Wed May 18 2016 Jitka Plesnikova <jplesnik@redhat.com> - 2.8.2-3
 - Perl 5.24 re-rebuild of bootstrapped packages