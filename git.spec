iusrepo/git224

commit 9125e652738865e75f2bddbcc698053eb1e67bf9

Use new INSTALL_SYMLINKS setting
Replace NO_CROSS_DIRECTORY_HARDLINKS and NO_INSTALL_HARDLINKS with
INSTALL_SYMLINKS.  The result is slightly improved; all symlinks will
point directly to the target rather than via multiple levels of
symlinks.

The rationale was covered in slightly more detail in d56cfc6 ("Use
symlinks instead of hardlinks for installed binaries", 2018-03-15).

Adjust the dangling-relative-symlink filter in the rpmlint config for
the new target of the git-difftool symlink.

git.spec
@@ -448,10 +448,9 @@ LDFLAGS = %{__global_ldflags}
 NEEDS_CRYPTO_WITH_SSL = 1
 USE_LIBPCRE = 1
 ETC_GITCONFIG = %{_sysconfdir}/gitconfig
+INSTALL_SYMLINKS = 1
 GITWEB_PROJECTROOT = %{_localstatedir}/lib/git
 GNU_ROFF = 1
-NO_CROSS_DIRECTORY_HARDLINKS = 1
-NO_INSTALL_HARDLINKS = 1
 NO_PERL_CPAN_FALLBACKS = 1
 %if %{with python2}
 PYTHON_PATH = %{__python2}
@@ -528,11 +527,6 @@ sed -i -e '1s@#!\( */usr/bin/env python\|%{__python2}\)$@#!%{__python3}@' \
 
 %install
 %make_install %{?with_docs:install-doc}
-
-# symlink %%{gitexecdir} copies of git, git-shell, and git-upload-pack
-for i in git git-shell git-upload-pack; do
-    ln -sf ../../bin/$i %{buildroot}%{gitexecdir}/$i
-done
 
 %global elispdir %{_emacs_sitelispdir}/git
 pushd contrib/emacs >/dev/null
@@ -879,6 +873,7 @@ make test || ./print-failed-test-output
 %changelog
 * Wed May 30 2018 Todd Zullinger <tmz@pobox.com> - 2.18.0-0.0.rc0
 - Update to 2.18.0-rc0
+- Use new INSTALL_SYMLINKS setting
 
 * Wed May 30 2018 Todd Zullinger <tmz@pobox.com> - 2.17.1-3
 - Use %%apply_patch for aarch64 zlib patch, return to %%autosetup

git.rpmlintrc
@@ -8,7 +8,7 @@ addFilter("git-subtree.* spelling-error %description .* (subdirectory|subproject
 addFilter("git(-core-doc)?\..*: W: dangling-relative-symlink /usr/share/doc/git/contrib/hooks ../../../git-core/contrib/hooks")
 
 # git requires git-core, which provides the symlink target
-addFilter("git\..*: W: dangling-relative-symlink /usr/libexec/git-core/git-difftool git")
+addFilter("git\..*: W: dangling-relative-symlink /usr/libexec/git-core/git-difftool ../../bin/git")
 
 # git-gui requires git, which provides the git binary
 addFilter("git-gui.noarch: W: desktopfile-without-binary /usr/share/applications/git-gui.desktop git")

commit bd7ea152d65c2cd500e99d07e7f168c9f562c5f4

fix build --without cvs
9125e65 ("Use new INSTALL_SYMLINKS setting", 2018-05-30) broke builds
using --without cvs. /usr/libexec/git-core/git-cvsserver became a
symlink instead of hardlink. Adapt the find command used to exclude
'git-cvs*' files to detect symlinks as well.

git.spec
@@ -83,7 +83,7 @@
 
 Name:           git
 Version:        2.18.0
-Release:        1%{?rcrev}%{?dist}
+Release:        2%{?rcrev}%{?dist}
 Summary:        Fast Version Control System
 License:        GPLv2
 URL:            https://git-scm.com/
@@ -560,7 +560,7 @@ find %{buildroot} Documentation -type f -name 'git-archimport*' -exec rm -f {} '
 
 %if ! %{with cvs}
 # Remove git-cvs* from %%{_bindir} and %%{gitexecdir}
-find %{buildroot}{%{_bindir},%{gitexecdir}} -type f -name 'git-cvs*' -exec rm -f {} ';'
+find %{buildroot}{%{_bindir},%{gitexecdir}} \( -type f -o -type l \) -name 'git-cvs*' -exec rm -f {} ';'
 %endif
 
 %if ! %{with p4}
@@ -864,6 +864,9 @@ make test || ./print-failed-test-output
 %{?with_docs:%{_pkgdocdir}/git-svn.html}
 
 %changelog
+* Mon Jun 25 2018 Pavel Cahyna <pcahyna@redhat.com> - 2.18.0-2
+- Fix build --without cvs
+
 * Wed Jun 20 2018 Todd Zullinger <tmz@pobox.com> - 2.18.0-1
 - Update to 2.18.0
 